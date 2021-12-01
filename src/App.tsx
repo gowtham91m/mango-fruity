af1f693e7bd9b8a72804b0c880d21ff1229d50dd
Gowtham Mallikarjuna <ggwm@amazon.com>
2021-11-30 17:54:11 -0800

    added global styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import GlobalStyle from "./GlobalStyle";
 import Drawings from "./Pages/Drawings";
 import HomePage from "./Pages/HomePage";
 import Photography from "./Pages/Photography";
@@ -7,18 +8,17 @@ import Programming from "./Pages/Programming";
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route path="/" exact component={HomePage} />
-          <Route path="/Photography" exact component={Photography} />
-          <Route path="/Programming" exact component={Programming} />
-          <Route path="/Drawings" exact component={Drawings} />
-          <HomePage />
-        </Switch>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <GlobalStyle />
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/Photography" exact component={Photography} />
+        <Route path="/Programming" exact component={Programming} />
+        <Route path="/Drawings" exact component={Drawings} />
+        <HomePage />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
